2210a3caf6d819e11a267d46c3986ca132348075
Naman Chhabra <nchhabra1311@gmail.com>
2024-04-25 03:48:56 +0530

    add api endpoints

--- src/apis/urls.py
+++ src/apis/urls.py
@@ -2,7 +2,10 @@ from apis import views
 
 
 api_urls = [
-    ("/", views.index, ["GET"], "flask scaffolding index url")
+    ("/", views.index, ["GET"], "flask scaffolding index url"),
+    ("/login", views.login, ["POST"], "flask scaffolding login url"),
+    ("/register", views.register_user, ["POST"], "flask scaffolding register user url"),
+
 ]
 
 other_urls = []
