7ebcf329462667f04a22fb20a7e7ab0bf3c331f2
Naman Chhabra <nchhabra1311@gmail.com>
2024-04-25 03:48:11 +0530

    create api logic

--- src/apis/views.py
+++ src/apis/views.py
@@ -1,8 +1,9 @@
 import logging
-
+from flask import request, abort
+from src.services import UserService
 
 logger = logging.getLogger("default")
-
+user_service = UserService()
 
 def index():
     logger.info("Checking the flask scaffolding logger")
@@ -17,4 +18,28 @@ def login():
            return a json response and an appropriate status code.
     """
 
-    pass
+    # Extract username and password from request
+    data = request.json
+    try:
+        username = data.get('username')
+        password = data.get('password')
+    except KeyError as e:
+        abort(400, e.args)
+
+    return UserService.login_user(username, password)
+
+def register_user():
+    """
+        API to sign up a user to test it's login
+    """
+    data = request.json
+    try:
+        username = data.get('username')
+        password = data.get('password')
+        display_name = data.get('display_name')
+        email = data.get('email')
+
+    except KeyError as e:
+        abort(400, e.args)
+
+    return UserService.register_user(username, password, email, display_name)
