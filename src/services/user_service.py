c0f4a91a6c225dacec1880ea19cff35e5803bb12
Naman Chhabra <nchhabra1311@gmail.com>
2024-04-26 04:56:52 +0530

    change return types and configure return objects

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,6 +1,6 @@
-from src.models import User
-import bcrypt
-from flask import jsonify, abort
+from models import User
+from flask import jsonify
+from tasks.user import update_last_login
 
 class UserService(object):
     """
@@ -18,27 +18,43 @@ class UserService(object):
         user = User.objects(username=username).first()
 
         # Check if user exists and validate password
-        if user and bcrypt.checkpw(password.encode('utf-8'), user.password_hash.encode('utf-8')):
-            return jsonify({"user": user, "status": 'LOGGED_IN'})
+        if user and user.password_hash == str(hash(password)):
+            user_id = user.id
+            user_obj = {
+                "name": user.display_name,
+                "email": user.email,
+                "user_id": user_id,
+            }
+
+            # Call Celery task to update last login
+            update_last_login.delay(user_id)
+
+            return jsonify({"user": user_obj, "status": 'LOGGED_IN', "last_login_at": user.last_login_at})
         else:
-            raise abort(400, "Incorrect username or password")
+            return jsonify({"error":"Incorrect username or password"}), 400
         
 
     def register_user(self, username, password, email, name):
 
         # Check if username or email already exists
         if User.objects(username=username).first():
-            raise abort(400, "Username already taken")
+            return jsonify({"error":"Username already taken"}), 400
+
         if User.objects(email=email).first():
-            raise abort(400, "Email already taken")
+            return jsonify({"error":"Email already taken"}), 400 
+
 
         # Hash the password
-        password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
+        password_hash = str(hash(password))
         # Create and save the new user
         user = User(username=username, email=email, password_hash=password_hash, display_name=name)
         user.save()
+        user_obj = {
+            "name": user.display_name,
+            "email": user.email,
+            "user_id": user.id,
+        }
 
-        return jsonify({"user": user, "status": 'SIGNED_UP'})
+        return jsonify({"user": user_obj, "status": 'SIGNED_UP'})
         
 
